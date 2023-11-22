decaf10f513e3f231db131b909d5516c88197ef2
Alex <73742087+Alexofthefoot@users.noreply.github.com>
2023-11-21 17:41:04 -0800

    form now updates values, but they can be messy

--- js/main.js
+++ js/main.js
@@ -3,26 +3,46 @@
   var waterProgress = 0; //store value in mL's, change the increment to L as needed
   var standardIncrement = 250;
   
-  function showForm() {
-
-  }
-
-  function editInformation(val) {
-    //show form
+  function openForm() {
+    //un-hide the form
     document.getElementById("water-form").style.display = "inline";
-    //set default values based on
+    //set default form values based on current data
     document.getElementById("form-goal").value = waterGoal;
     document.getElementById("form-prog").value = waterProgress;
     document.getElementById("form-incr").value = standardIncrement;
-
-    // Adjust values based on input
-  
   }
 
-  // Changes what is displayed on the page
-  function displayGoal() {
+  // This might help: https://javascript.info/formdata
 
+  // Update stored data based on form input. If blank, makes no change
+ function closeForm() {  
+    var goalInput = document.getElementById("form-goal").value;
+    if (goalInput != null) {
+      waterGoal = goalInput;
+    }
+    var progInput = document.getElementById("form-prog").value;
+    if (progInput != null) {
+      waterProgress = progInput;
+    }
+    var incrInput = document.getElementById("form-incr").value;
+    if (incrInput != null) {
+      standardIncrement = incrInput;
+    }
+    //hide form
+    document.getElementById("water-form").style.display = "none";
+    displayProgress();
+    displayGoal();
   }
+
+  // document.getElementById("water-form").onsubmit = function(e){
+  //   var fieldValue = document.getElementByID("username").value;
+  //   if(fieldValue == null || fieldValue == ""){
+  //     // the field was empty. Stop form submission
+  //     e.preventDefault();
+  //     // Now tell the user something went wrong
+  //     alert("you must enter a username");
+  //     }
+  //   }
 
   // Progress is increased by a standard increment
   function increaseProgress() {
@@ -30,7 +50,17 @@
     displayProgress();
   }
 
-  // Changes what is displayed on the page
+  function displayGoal() {
+    //adjust display
+    if (waterGoal > 999) {
+      document.getElementById("goal").innerHTML = waterGoal/1000 + "L";
+    }
+    else {
+      document.getElementById("goal").innerHTML = waterGoal + "mL";
+    }
+  }
+
+  // Updates what is displayed on the page
   function displayProgress() {
     //set initial values
     waterIncrement = "mL";
@@ -40,19 +70,17 @@
       waterIncrement = "L"
       waterDisplay = waterProgress/1000;
     }
-
     //adjust display
     document.getElementById("progress").innerHTML = waterDisplay + waterIncrement;
-
     //Check if goal has been reached
     if (waterProgress >= waterGoal) {
       document.getElementById("reachgoal").innerHTML = "Goal Reached, great job!";
-
     }
   }
 
   function reset() {
     waterProgress = 0;
+    waterGoal = 2500;
     displayProgress();
     // hide the form
     document.getElementById("water-form").style.display = "none";
