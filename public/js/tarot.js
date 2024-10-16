b9a12c504136fc89cca4d3dee11e41c637fd9f8b
Alex <73742087+Alexofthefoot@users.noreply.github.com>
2024-10-15 18:37:39 -0700

    beginnings of landing page

--- public/js/tarot.js
+++ public/js/tarot.js
@@ -1,25 +1,53 @@
-// JavaScript to handle form submission
-document.getElementById('tarot-new-card').addEventListener('submit', function(event) {
-    event.preventDefault(); // Prevent the default form submission
+// import the variables and function from module.js
+import { db } from './app.js';
 
-    const formData = new FormData(this);
-    const data = Object.fromEntries(formData.entries());
+function insertTarotReading() {
+    const sql = 'INSERT INTO cards (deck_id, arcana, card_name, card_value) VALUES (?, ?, ?)';
 
-    // Send data to the server
-    fetch('/add-card', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(data)
-    })
-    .then(response => response.json())
-    .then(result => {
-        console.log('Success:', result);
-        alert('Card added successfully!');
-    })
-    .catch(error => {
-        console.error('Error:', error);
-        alert('Error adding card.');
+    db.query(sql, [cards.deck_id, cards.arcana, cards.card_name, cards.card_value], (err, result) => {
+        if (err) {
+            console.error('Error inserting tarot card:', err);
+            return;
+        }
+        console.log('Tarot card inserted successfully:', result);
     });
-});
+}
+
+// const reading = {
+//     deck_id : 1,
+//     arcana : "Major",
+//     card_name : "The Fool",
+//     card_value : 1,
+// }
+
+// insertTarotReading(reading);
+
+
+
+
+
+// // JavaScript to handle form submission
+// document.getElementById('tarot-new-card').addEventListener('submit', function(event) {
+//     event.preventDefault(); // Prevent the default form submission
+
+//     const formData = new FormData(this);
+//     const data = Object.fromEntries(formData.entries());
+
+//     // Send data to the server
+//     fetch('/add-card', {
+//         method: 'POST',
+//         headers: {
+//             'Content-Type': 'application/json'
+//         },
+//         body: JSON.stringify(data)
+//     })
+//     .then(response => response.json())
+//     .then(result => {
+//         console.log('Success:', result);
+//         alert('Card added successfully!');
+//     })
+//     .catch(error => {
+//         console.error('Error:', error);
+//         alert('Error adding card.');
+//     });
+// });
