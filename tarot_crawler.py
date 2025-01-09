9bdf85a04e9eb7a3b624e91b8c2b2b4fe5ceaef5
Alex <73742087+Alexofthefoot@users.noreply.github.com>
2025-01-08 19:36:20 -0800

    progress on tarot image crawler

--- tarot_crawler.py
+++ tarot_crawler.py
@@ -1,41 +1,57 @@
-# === url_crawler.py === #
+# === Imports === #
 import requests
+import pandas as pd
 from bs4 import BeautifulSoup
+from selenium import webdriver
+from selenium.webdriver import ChromeOptions
 
-urls = []
+# === Constants === #
+BASE_URL = "https://sacred-texts.com/tarot/xr/index.htm"
 
-# Collect links from main page
-
-response = requests.get(f"https://sacred-texts.com/tarot/xr/index.htm")
-html_doc = BeautifulSoup(response.text, "html.parser")
-print("Loop through each link on the main page")
-for link in html_doc.find_all('a', href=True):
-    if len(link['href']) <= 9:
-            urls.append(link['href'])
-    else:
-        print("URL rejected: " + link['href'])
-if len(urls) != 78:
-    print("INCOMPLETE DECK: " + len(urls) + " links stored")
-else:
-    print("78 links stored.")
-
-
-# Save the final path segments to cards.txt
-
-with open("cards.txt", "w") as f:
-    for url in urls:
-        f.write(url + "\n")
-
-# Visit each subpage 
-
-print("visit each link")
-
-for link in urls:
-    response = requests.get(f"https://sacred-texts.com/tarot/xr/{link}")
-    html_doc2 = BeautifulSoup(response.text, "html.parser")
-print(count, "pages visited.")
-
-
+# === Function Definitions === #
+def scrape_images(urls):
+    options = ChromeOptions()
+    options.add_argument("--headless=new")
+    driver = webdriver.Chrome(options=options)
+    #driver = webdriver.Chrome()
+    for link in urls:
+        driver.get(f"https://sacred-texts.com/tarot/xr/{link}")
+        results = []
+        content = driver.page_source
+        soup = BeautifulSoup(content, "html.parser")
+        title = soup.select_one('h1')
+        print(title.text)
 
     
+def scrape_urls():
+    urls = []
 
+    response = requests.get(BASE_URL)
+    html_doc = BeautifulSoup(response.text, "html.parser")
+    for link in html_doc.find_all('a'):
+        href = link.get('href')
+        if len(href) == 8:
+            urls.append(href)
+    return urls
+
+def save_to_file(urls):
+    with open("cards.txt", "w") as f:
+        for url in urls:
+            f.write(url + "\n")
+    
+# === Main Function === #
+def main():
+    print("Starting scraper...")
+    urls = scrape_urls()
+    print(len(urls), "Urls found.")
+    save_to_file(urls)
+    print("Saved to file.")
+    print("Visiting webpages...")
+    scrape_images(urls)
+    
+    
+    
+
+# === Script Entry Point === #
+if __name__ == "__main__":
+    main()
