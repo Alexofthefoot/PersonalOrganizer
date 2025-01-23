dfd8c224cad4f3361b6848ccdb46bc5d7976681a
Alex <73742087+Alexofthefoot@users.noreply.github.com>
2025-01-22 16:31:16 -0800

    changed image names to be more accessible

--- public/js/tarot/new-reading.js
+++ public/js/tarot/new-reading.js
@@ -1,7 +1,28 @@
 var num_cards;
 
 function deal_cards() {
+    // Randomize suit
+    const suits = ["ar", "cu", "sw", "pe", "wa"];
+    var suit = suits[Math.floor(Math.random() * suits.length)];
 
+    //Randomize card
+    const num = []
+    var random = Math.floor(Math.random() * 5) // between 0 and 3 (inclusive)
+    console.log(random)
+    
+    // randomize card number (ace to king)
+    random = Math.floor(Math.random() * 13)
+    var num
+    if (random == 0) {
+        num = "ac"
+    }
+    else if (random < 10 && random > 1){//2 to 9
+        num == "0" + random.toString()
+    }
+    else if (random == 10){
+        num = "10"
+    }
+    else
 }
 
 function flipCard(num) {
@@ -9,4 +30,12 @@ function flipCard(num) {
     console.log("flipCard triggered");
     // document.getElementById(id).style.display = "none";
     document.getElementById(id).src = "../../../images/cards/ar01.png"
+    deal_cards()
 }
+
+// function hand_size(num) {
+//     if (num == 1){
+
+//     }
+
+// }
--- tarot_crawler.py
+++ tarot_crawler.py
@@ -11,18 +11,32 @@ from selenium.webdriver import ChromeOptions
 
 # === Constants === #
 BASE_URL = "https://sacred-texts.com/tarot/xr/index.htm"
-
+PATH = "public/images/cards/"
 
 # === Function Definitions === #
+def fix_card_num(str):
+    match str:
+        case "ac":
+            str = "01"
+        case "pa":
+            str = "11"
+        case "kn":
+            str = "12"
+        case "qu":
+            str = "13"
+        case "ki":
+            str = "14"
+        case _:
+            pass
+    return str
 # Downloads the img
 def extract_images(images):
     for i in images:
-        
         image_content = requests.get(i).content
         image_file = io.BytesIO(image_content)
         image = Image.open(image_file).convert("RGB")
-        card_name = i[39:43]
-        file_path = Path("public/images/" + card_name + ".png")
+        card_name = i[39:41] + fix_card_num(i[41:43])     
+        file_path = Path(PATH + card_name + ".png")
         image.save(file_path, "PNG", quality=80)
     
 # Visit each card page and save the src address for img
@@ -41,10 +55,7 @@ def collect_images(urls):
         if src not in images:
             address = src.replace("../","https://sacred-texts.com/tarot/")
             images.append(address)
-    driver.quit()
-    return images
-    
-
+    driver.quit() return images
 
 # Collects the url extentions for each card page
 def collect_urls():
