09f3176a469a0ed0a4870abba338a8bd2b6e43ac
Alex <73742087+Alexofthefoot@users.noreply.github.com>
2025-01-22 17:05:29 -0800

    3 cards deal with no repeats

--- public/js/tarot/new-reading.js
+++ public/js/tarot/new-reading.js
@@ -1,17 +1,23 @@
 var num_cards;
+let currentCards = ["", "", "", "", ""]
 
 function deal_card() {
+    var dealt_card
     // Randomize suit
     const suits = ["ar", "cu", "sw", "pe", "wa"];
-    var suit = suits[Math.floor(Math.random() * suits.length)];
+    dealt_card = suits[Math.floor(Math.random() * suits.length)];
 
     // randomize card (ace to king)
     card = Math.floor(Math.random() * 13 + 1).toString()
     if (card < 10) {
         card = "0" + card
     }
-    // console.log(suit+card)
-    return suit+card
+    dealt_card += card
+    while (currentCards.includes(dealt_card)) {
+        console.log("duplicate card skipped")
+        dealt_card = deal_card()
+    }
+    return dealt_card
 }
 
 function flipCard(num) {
@@ -19,6 +25,7 @@ function flipCard(num) {
     console.log("flipCard triggered");
     // document.getElementById(id).style.display = "none";
     newcard = deal_card()
+    currentCards[num-1] = newcard
     document.getElementById(id).src = "../../../images/cards/" + newcard + ".png"
 }
 
