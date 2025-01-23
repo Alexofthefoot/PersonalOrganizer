60dd5431f32ce0004b1086115adf30282a32ca70
Alex <73742087+Alexofthefoot@users.noreply.github.com>
2025-01-22 16:37:42 -0800

    3 cards flip over when clicked

--- public/js/tarot/new-reading.js
+++ public/js/tarot/new-reading.js
@@ -1,41 +1,27 @@
 var num_cards;
 
-function deal_cards() {
+function deal_card() {
     // Randomize suit
     const suits = ["ar", "cu", "sw", "pe", "wa"];
     var suit = suits[Math.floor(Math.random() * suits.length)];
 
-    //Randomize card
-    const num = []
-    var random = Math.floor(Math.random() * 5) // between 0 and 3 (inclusive)
-    console.log(random)
-    
-    // randomize card number (ace to king)
-    random = Math.floor(Math.random() * 13)
-    var num
-    if (random == 0) {
-        num = "ac"
+    // randomize card (ace to king)
+    card = Math.floor(Math.random() * 13 + 1).toString()
+    if (card < 10) {
+        card = "0" + card
     }
-    else if (random < 10 && random > 1){//2 to 9
-        num == "0" + random.toString()
-    }
-    else if (random == 10){
-        num = "10"
-    }
-    else
+    // console.log(suit+card)
+    return suit+card
 }
 
 function flipCard(num) {
     var id= 'card' + num
     console.log("flipCard triggered");
     // document.getElementById(id).style.display = "none";
-    document.getElementById(id).src = "../../../images/cards/ar01.png"
-    deal_cards()
+    newcard = deal_card()
+    document.getElementById(id).src = "../../../images/cards/" + newcard + ".png"
 }
 
 // function hand_size(num) {
-//     if (num == 1){
-
-//     }
 
 // }
