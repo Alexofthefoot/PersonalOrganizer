dfb121d454b41daa9060f678d63ba0cf974f0a36
Alex <73742087+Alexofthefoot@users.noreply.github.com>
2025-01-23 17:46:37 -0800

    ready to connect tarot page to DB

--- public/js/tarot/new-reading.js
+++ public/js/tarot/new-reading.js
@@ -14,44 +14,57 @@ function deal_card() {
         card = "0" + card
     }
     dealt_card += card
+    // Avoids any duplicate cards
     while (currentCards.includes(dealt_card)) {
-        console.log("duplicate card skipped")
         dealt_card = deal_card()
     }
     return dealt_card
 }
 
-// Flips or refreshes the card that was clicked 
+// Flips the card that was clicked 
 function flipCard(num) {
-    var id= 'card' + num
-    // console.log("flipCard triggered");
+    // Does nothing if the card has already been dealt      // TODO: 
+    if (currentCards[num-1] != "") {
+        return;
+    }
+
+    var id = 'card' + num
+    var element = document.getElementById(id)
     newcard = deal_card()
     currentCards[num-1] = newcard
     console.log(currentCards)
-    document.getElementById(id).src = "../../../images/cards/" + newcard + ".png"
+    element.src = "../../../images/cards/" + newcard + ".png"
 }
 
 function changeHandSize(num) {
     num_cards = num;
+    var element
     for (let i = 1; i <= 5; i++){
+        element = document.getElementById("card"+ i)
         if (i <= num_cards) {
             // make visible
-            console.log("make "+ i + "visible")
-            document.getElementById("card"+ i).style.display = "inline";
+            element.style.display = "inline";
         }
         else {
             // make invisible & remove from hand
-            console.log("make "+ i + "invisible")
-            document.getElementById("card" + i).style.display = "none";
-            document.getElementById("card" + i).src = "../../images/card back black.png"
+            element.style.display = "none";
+            element.src = "../../images/card back black.png"
             currentCards[i-1] = ""
         }
     }      
 }
+
 function resetHand() {
     for (let i = 1; i <= 5; i++){
         document.getElementById("card" + i).src = "../../images/card back black.png"
         currentCards[i-1] = ""     
     }   
-    console.log(currentCards)   
+    console.log(currentCards)  
+    document.getElementById("user-message").innerHTML = "Flip over cards to uncover your reading!"
+}
+
+function saveReading() {
+    // TODO: attach to DB
+    el = document.getElementById("user-message")
+    el.innerHTML = "Oops, that doesnt exist yet"
 }
